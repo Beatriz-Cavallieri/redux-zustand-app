b03a7f94d90c045f662f3295231f373e23d76933
Beatriz Cavallieri <bfcavallieri@gmail.com>
2025-02-25 13:24:35 -0300

    Installs Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { Provider } from "react-redux"
 import { store } from "./store"
 import ToDoList from "./components/ToDoList"
 import AddToDo from "./components/AddToDo"
+import './styles/global.css'
 
 function App() {
 
   return (
-    <Provider store={store}>
-      <ToDoList />
-      <AddToDo />
-    </Provider>
+      <Provider store={store}>
+        <ToDoList />
+        <AddToDo />
+      </Provider>
   )
 }
 
